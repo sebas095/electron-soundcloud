4716621a19891df1aa514968a74f78e22077f2ec
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-12-29 21:26:52 -0500

    hello world

---
+++ app/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+
+const container = document.getElementById('component');
+ReactDOM.render(<App />, container);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const debug = false;
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./app/app.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/public/js/",
+    filename: "bundle.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
