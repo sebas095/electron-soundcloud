92886d6b1af38db9aab55750486f268d217f2cfc
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-12-29 21:59:04 -0500

    Integrate components

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import Details from '../components/Details';
+import Player from '../components/Player';
+import Progress from '../components/Progress';
+import Search from '../components/Search';
+
 export default class App extends Component {
   constructor() {
     super();
   }
-  
+
   render() {
     return (
       <div>
-        Hola Mundo
+        <Search />
+        <Details />
+        <Player />
+        <Progress />
       </div>
     );
   }
